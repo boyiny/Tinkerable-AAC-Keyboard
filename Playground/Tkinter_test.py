383eaf2ba884757312b43cb89f13e4e1ea5030b2
TuringFallAsleep <by266@cam.ac.uk>
2021-12-02 21:38:32 +0000

    drawable keys but with layout issue

--- Playground/Tkinter_test.py
+++ Playground/Tkinter_test.py
@@ -3,13 +3,26 @@
 from tkinter import *
 import tkinter as tk
 
+# from tkinter.dnd import Tester as DragWindow, Icon as Dragable
+
 from generate_keyboard import *
+
+
 
 Keyboard_App = tk.Tk()
 Keyboard_App.title("Tkinter Onscreen Keyboard")
-Keyboard_App.resizable(0,0)
+Keyboard_App.resizable()
+# Keyboard_App.grid_rowconfigure(0, weight=1)
+# Keyboard_App.grid_columnconfigure(0, weight=1)
 
+# frame = DnDFrame(Keyboard_App, bd=4, bg="grey")
+# frame.place(x=10, y=10)
+# make_draggable(frame)
 
+# notes = tk.Text(frame)
+# notes.pack()
+
+# textFrame = DnDFrame(Keyboard_App, bd=4)
 entry = Entry(Keyboard_App, width=110, font=('Calibri', 18))
 entry.grid(row=0, columnspan=15)
 
