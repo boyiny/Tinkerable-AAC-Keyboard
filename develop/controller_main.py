a287f561df798e21132a534b2421ed463eb3daa5
TuringFallAsleep <by266@cam.ac.uk>
2021-12-10 20:42:41 +0000

    MVC architecture applied

---
+++ develop/controller_main.py
@@ -0,0 +1,22 @@
+from model_main import *
+from view_main import *
+
+
+class Controller_main():
+    def __init__(self):
+        self.modelMain = Model_main()
+        self.viewMain = View_main(self)
+        
+
+
+    def main(self):
+        self.viewMain.mainloop()
+
+    def on_button_click(self, caption):
+        text = self.modelMain.edit_text(caption)
+        self.viewMain.textBox.set(text)
+  
+
+if __name__ == '__main__':
+    keyboard = Controller_main()
+    keyboard.main()
