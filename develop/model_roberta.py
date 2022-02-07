777738e65d2ce862597c400ca733f96cebc66eee
TuringFallAsleep <by266@cam.ac.uk>
2022-02-07 23:32:20 +0000

    clear the file

--- develop/model_roberta.py
+++ develop/model_roberta.py
@@ -1,17 +1,21 @@
 from transformers import pipelines
 from transformers import RobertaTokenizer, RobertaForMaskedLM
 import torch
+import re
 
 
 class Model_Roberta:
+    
+
     def __init__(self):
         self.unmasker = pipelines.pipeline('fill-mask', model='roberta-base')
+        
 
         self.tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
         self.model = RobertaForMaskedLM.from_pretrained('roberta-base')
         
 
-    def predict_next_few_words (self, sent, num):
+    def predict_next_few_words(self, sent, num):
 
         for i in range(num):
             sent = sent + "<mask>"
@@ -28,10 +32,10 @@ class Model_Roberta:
         list_of_list =[]
         for index, mask_index in enumerate(masked_pos):
             mask_hidden_state = last_hidden_state[mask_index]
-            idx = torch.topk(mask_hidden_state, k=5, dim=0)[1]
+            idx = torch.topk(mask_hidden_state, k=100, dim=0)[1]
             words = [self.tokenizer.decode(i.item()).strip() for i in idx]
             list_of_list.append(words)
-            print ("Mask ",index+1,"Guesses : ",words)
+            # print ("Mask ",index+1,"Guesses : ",words)
         
         best_guess = ""
         for j in list_of_list:
@@ -39,21 +43,26 @@ class Model_Roberta:
             
         return best_guess
 
-    def predict_next_word(self, query):
+    def predict_words(self, query):
         text = query + "<mask>"
-        result = self.unmasker(text)
-        return result
+        results = self.unmasker(text)
+        wordsPred = []
+        for element in results:
+            elementWithoutSymbol = re.sub(r'[^\w]', '', element['token_str'])
+            if elementWithoutSymbol != '':
+                wordsPred.append(elementWithoutSymbol)
+        return wordsPred
 
 
 if __name__ == '__main__':
     roberta = Model_Roberta()
     query = "windy London"
-    result = roberta.predict_next_word(query)
+    result = roberta.predict_words(query)
     for res in result:
         print(f"First pred: {res}")
 
     query = "merry christmas"
-    result = roberta.predict(query)
+    result = roberta.predict_words(query)
     for res in result:
         print(f"Second pred: {res}")
 
@@ -61,10 +70,13 @@ if __name__ == '__main__':
     sentence = "windy London"
     numOfPredWords = 3
 
-    predicedSentence = roberta.get_prediction(sentence, numOfPredWords)
+    predicedSentence = roberta.predict_next_few_words(sentence, numOfPredWords)
     print ("\nBest guess for fill in the blank :::", predicedSentence)
 
-
+    while(True):
+        query = input("Input your query: ")
+        wordsPred = roberta.predict_words(query)
+        print(f"Result: {wordsPred}")
 
 
 
